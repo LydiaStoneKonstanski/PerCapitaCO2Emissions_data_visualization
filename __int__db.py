8f78ce38d72b0b1ed9536543b860cb68b1632b3b
LydiaStoneKonstanski <yourname@example.com>
2024-04-24 16:40:30 -0400

    There are things to revisit on initdb but the structure is there.

--- __int__db.py
+++ __int__db.py
@@ -2,6 +2,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from os import environ, path
+import mysql.connector
+
 
 basedir = path.abspath(path.dirname(__file__))
 #TODO is the below important?
@@ -15,3 +17,36 @@ class Config:
     SQALCHEMY_DATABASE_URI = 'mysql+pymysql://lydia:celeste@localhost/co2'
     SQALCHEMY_ECHO = False
     SQALCHEMY_TRACK_MODIFICATIONS = False
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='lydia',
+    passwd='celeste',
+)
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute('SHOW DATABASES')
+for db in my_cursor:
+    print(db)
+#TODO insert name of database here:
+my_cursor.execute('USE ???')
+
+#TODO I don't want sqlite3. Decide what to do instead
+#import sqlite3
+#import pymysql
+
+#connection = sqlite3.connect('../database.db')
+
+
+# with open('schema.sql') as f:
+#     connection.executescript(f.read())
+#
+# cur = connection.cursor()
+#my_cursor.execute("INSERT INTO blogposts (title, content) VALUES (%s, %s)",
+            ('First Post', 'Content for the first post')
+                  )
+
+mydb.commit()
+# connection.commit()
+# connection.close()
