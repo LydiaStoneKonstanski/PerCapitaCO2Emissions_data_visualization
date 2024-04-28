0dd74fbe6b3b7a54822963a6c36846948469b8d3
LydiaStoneKonstanski <yourname@example.com>
2024-04-28 16:50:36 -0400

    Country Buttons, Generating radar graphs.

--- co2_viz.py
+++ co2_viz.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
 from markupsafe import escape
 
+wiki_links = {
+    "Australia": "https://en.wikipedia.org/wiki/Australia",
+    "United States": "https://en.wikipedia.org/wiki/United_States"
+}
+
 app = Flask(__name__, template_folder="html_files")
 
 # @app.route("/<name>")
@@ -30,6 +35,22 @@ def attributions():
 @app.route("/beautiful-web-type")
 def beautifulwebtype():
     return render_template("beautiful-web-type")
+
+@app.route("/country/<selected_country>")
+def country(selected_country):
+    wiki_link = wiki_links[selected_country]
+    sidebar = get_wiki_sidebar(wiki_link)
+    return render_template("country.html", country=selected_country, sidebar=sidebar, wiki_link=wiki_link)
+
+# TESTING
+from bs4 import BeautifulSoup
+import requests
+
+def get_wiki_sidebar(wiki_link):
+    page = requests.get(wiki_link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find(class_="infobox ib-country vcard")
+    return result
 
 
 app.run(debug=True)
