9c1d2ef1a6904043ca3905c17e25ab4306ddeb0a
LydiaStoneKonstanski <yourname@example.com>
2024-04-28 17:47:18 -0400

    Country pages, wikipedia as placeholder data

--- co2_viz.py
+++ co2_viz.py
@@ -3,6 +3,24 @@ from markupsafe import escape
 
 wiki_links = {
     "Australia": "https://en.wikipedia.org/wiki/Australia",
+    "Austria": "https://en.wikipedia.org/wiki/Austria",
+    "Belgium": "https://en.wikipedia.org/wiki/Belgium",
+    "Canada": "https://en.wikipedia.org/wiki/Canada",
+    "China": "https://en.wikipedia.org/wiki/China",
+    "Czechia": "https://en.wikipedia.org/wiki/Czechia",
+    "Germany": "https://en.wikipedia.org/wiki/Germany",
+    "Ireland": "https://en.wikipedia.org/wiki/Republic_of_Ireland",
+    "Japan": "https://en.wikipedia.org/wiki/Japan",
+    "Kazakhstan": "https://en.wikipedia.org/wiki/Kazakhstan",
+    "Libya": "https://en.wikipedia.org/wiki/Libya",
+    "Malaysia": "https://en.wikipedia.org/wiki/Malaysia",
+    "Netherlands": "https://en.wikipedia.org/wiki/Netherlands",
+    "Norway": "https://en.wikipedia.org/wiki/Norway",
+    "Poland": "https://en.wikipedia.org/wiki/Poland",
+    "Saudi Arabia": "https://en.wikipedia.org/wiki/Saudi_Arabia",
+    "Singapore": "https://en.wikipedia.org/wiki/Singapore",
+    "Turkmenistan": "https://en.wikipedia.org/wiki/Turkmenistan",
+    "United Arab Emirates": "https://en.wikipedia.org/wiki/United_Arab_Emirates",
     "United States": "https://en.wikipedia.org/wiki/United_States"
 }
 
@@ -50,7 +68,12 @@ def get_wiki_sidebar(wiki_link):
     page = requests.get(wiki_link)
     soup = BeautifulSoup(page.content, 'html.parser')
     result = soup.find(class_="infobox ib-country vcard")
-    return result
+    if result is None:
+        # Netherlands has a different class name
+        result = soup.find(class_="infobox ib-pol-div vcard")
+
+    html = str(result).replace('href="/wiki', 'href="https://en.wikipedia.org/wiki')
+    return html
 
 
 app.run(debug=True)
